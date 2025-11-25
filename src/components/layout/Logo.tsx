e4cbd648fb6abbb72c0016a56d0f71ea08a0e4e9
naren4488 <narendrakajla77@gmail.com>
2025-11-25 14:15:51 +0530

    logo updated

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -4,18 +4,22 @@ import Image from "next/image";
 export function Logo() {
   return (
     <Link href="/" className="flex items-center group h-full">
-      <div className="flex items-center gap-2 px-4 py-2 h-full bg-white rounded-r-full shadow-sm group-hover:shadow-md transition-shadow">
-        <div className="relative w-36 h-10 shrink-0">
+      <div className="flex items-center gap-0 px-4 py-2 h-full bg-white rounded-r-full shadow-sm group-hover:shadow-md transition-shadow">
+        <div className="relative w-10 h-10 shrink-0">
           <Image
-            src="/assets/logo.svg"
+            src="/assets/icons/mahisolarenergylogo.svg"
             alt="Mahi Solar Energy Logo"
             fill
             className="w-full h-full"
             priority
           />
         </div>
+        <p className="font-bold uppercase text-primary leading-none">
+          MAHI SOLAR
+          <br/>
+          ENERGY
+        </p>
       </div>
     </Link>
   );
 }
-
